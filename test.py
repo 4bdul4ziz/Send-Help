a21a24dfab370d74b14f29de2f7eb252fc1f68c9
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2021-09-11 04:38:30 +0530

    Update test.py
    shhh

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://discord.com/api/webhooks/875657119490244619/1ewJuQTfS1lY78Q_5Io4q4ZAeVrB8fV4uQIKU_-CbaUR6ytgIcTnP-dH2z8NvAGZ1mYa"
+url = ""
 
 while True:
     message = input()
