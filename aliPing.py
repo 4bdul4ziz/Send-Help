57e4ebc8e0a04862b79a56feb5fb9f1d0085eff1
Abdul Aziz <bb.abdulaziz@icloud.com>
2021-09-17 13:30:46 +0530

    bing

--- aliPing.py
+++ aliPing.py
@@ -1,12 +1,12 @@
 import time
 from pynput.keyboard import Key, Controller
 
-print("Pinging Ali in 7 seconds...")
+print("Pinging in 7 seconds...")
 i = 1
 time.sleep(7)
 while i:
 
-    text = "<@388753316848336896>"
+    text = "<@600653864634679297>"
 
     keyboard = Controller()
 
