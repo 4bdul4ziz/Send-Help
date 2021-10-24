c3cae4e3bbdbd2affcbc17b1afb31ea06f34b119
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2021-10-24 17:13:39 +0530

    lol

--- m.py
+++ m.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://discord.com/api/webhooks/894261380880949309/V3o3-LYLXA-XpJA3CmCm5vXhz1-9EO5Rsnp8QwwJ4ETJnFC2T2Z3BhifkoU6UqzYmLfn"
+url = "https://discord.com/api/webhooks/896126003464794142/W4Jf9FxxJ_YyZo2orTwJo5CFmcn-SIpkzOlT5BteUcx-egyRdFYMQ8q_fgG0ImjB-Kek"
 while True:
     message = input()
     r = requests.post(url, data = {"content": message})
