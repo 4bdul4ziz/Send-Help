51af852d1c5b3714e796ed72eb7bd35cc24a7da4
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2023-04-18 21:13:01 +0530

    idk

--- m.py
+++ m.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://canary.discord.com/api/webhooks/978369026998796329/0jRhqTfZXMl-2Sr8jXeE2zIA-N0KX1E3kqOum0pzFOwWlS8_pyp_qdTXlGekLYTLF88v"
+url = "https://discord.com/api/webhooks/1097562508899930152/V0xQ7yeTrtBwCqNWTZPFO12-YU60HE9BEqXv-DWKBrigCe71onoBjtpjrvFmbOpKLq63"
 while True:
     message = input()
     r = requests.post(url, data={"content": message})
